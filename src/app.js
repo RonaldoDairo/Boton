32b9755f78224648e2d17135c9a2d61152dd3645
RonaldoDairo <programaciondairo@gmail.com>
2023-02-10 01:51:00 +0100

    Repositorio de boton random

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const output = document.querySelector('#excuse');
+  const button = document.getElementById("btn");
+
+  button.addEventListener("click", function() {
+    output.innerHTML = Generateexcuse();
+  });
+};
+
+let Generateexcuse = () => {
+  let who = ['my sister', 'my granma', 'his cat', 'my bird'];
+  let what = ['eat', 'pissed', 'crushed', 'broke'];
+  let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+
+  return who[whoindex] + ' ' + what[whatindex] + ' ' + when[whenindex];
 };
