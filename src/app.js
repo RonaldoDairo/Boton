ff82b2b6a2312047fbad393cd24982e1716c821c
RonaldoDairo <programaciondairo@gmail.com>
2023-02-10 01:31:30 +0000

    Agregando algunas modificaciones

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  const output = document.querySelector('#excuse');
+  const output = document.querySelector("#excuse");
   const button = document.getElementById("btn");
 
   button.addEventListener("click", function() {
@@ -8,13 +8,19 @@ window.onload = function() {
 };
 
 let Generateexcuse = () => {
-  let who = ['my sister', 'my granma', 'his cat', 'my bird'];
-  let what = ['eat', 'pissed', 'crushed', 'broke'];
-  let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying'];
+  let who = ["my sister", "my granma", "his cat", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broke"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
   let whoindex = Math.floor(Math.random() * who.length);
   let whatindex = Math.floor(Math.random() * what.length);
   let whenindex = Math.floor(Math.random() * when.length);
 
-  return who[whoindex] + ' ' + what[whatindex] + ' ' + when[whenindex];
+  return who[whoindex] + " " + what[whatindex] + " " + when[whenindex];
 };
